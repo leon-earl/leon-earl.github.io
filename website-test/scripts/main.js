fd4210fd41048cf6969f2c92e1e35c992c4ec459
leon-earl <39905276+leon-earl@users.noreply.github.com>
2018-06-05 13:42:46 +0100

    Add files via upload

---
+++ website-test/scripts/main.js
@@ -0,0 +1,20 @@
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h2');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to our Death Metal page, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome back to our Death Metal page, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
